9a7a6c503de69ba9596bfa02d3a1e1281ef61401
Yu Hsun Lee <andylee0914.tw@gmail.com>
2017-05-17 23:03:34 +0800

    fix send message error

--- app.js
+++ app.js
@@ -417,7 +417,23 @@ function receivedAccountLink(event) {
 }
 
 
+/*
+ * Send a text message using the Send API.
+ *
+ */
+function sendTextMessage(recipientId, messageText) {
+  var messageData = {
+    recipient: {
+      id: recipientId
+    },
+    message: {
+      text: messageText,
+      metadata: "DEVELOPER_DEFINED_METADATA"
+    }
+  };
 
+  callSendAPI(messageData);
+}
 
 
 
