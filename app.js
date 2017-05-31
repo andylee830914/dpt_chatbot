8c74987eb9e0e376424235078226fdf17a537d35
Yu Hsun Lee <andylee0914.tw@gmail.com>
2017-05-31 20:01:13 +0800

    change video source

--- app.js
+++ app.js
@@ -305,7 +305,7 @@ function receivedMessage(event) {
         sendTextMessage(senderID, "你才肥宅，你全家都肥宅!");
         break;
       case '嘿柴犬':
-        sendVideoMessage(senderID, 'gandog.mp4')
+        sendVideoMessage(senderID, 'gandog.mov')
         break;
       case '李昱勳':
         sendTextMessage(senderID, "我是個87");
