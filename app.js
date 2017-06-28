41f87019287c5397eca11306210c51dbb373207f
Yu Hsun Lee <andylee0914.tw@gmail.com>
2017-06-28 10:39:40 +0800

    fix json bug

--- app.js
+++ app.js
@@ -670,6 +670,7 @@ function callGraphAPI(recipientId) {
     if (!error && response.statusCode == 200) {
       var firstname = body.first_name;
       var lastname = body.last_name;
+      body = JSON.parse(body);
       console.log(body, lastname, body.last_name);
       if (lastname) {
         console.log("Successfully sent message with id %s to recipient %s",
